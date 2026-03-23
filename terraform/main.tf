29af11c23ef74bcaf9d36e80cc9c2c7e96f55f13
Nikhil <nikhilnareddula@gmail.com>
2026-03-23 14:48:27 +0530

    fix: close missing bracket in terraform required_providers block

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,10 +7,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-     # --- FOR ARCHIVE BLOCK ---
+    # --- FOR ARCHIVE BLOCK ---
     archive = {
       source  = "hashicorp/archive"
       version = "~> 2.4.0"
+    }
   }
 }
 
