5df1633837d106c1ed83df571b63da1e2ada2bf9
Nikhil <nikhilnareddula@gmail.com>
2026-03-24 14:35:42 +0530

    security: encrypt SNS topic at rest and document X-Ray risk acceptance

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -1,6 +1,9 @@
 # 1. Create the Notification Hub (The Topic)
 resource "aws_sns_topic" "remediation_alerts" {
   name = "zero-tolerance-finops-alerts"
+
+   # --- THE FIX: Encrypt messages at rest using the free AWS managed key ---
+  kms_master_key_id = "alias/aws/sns" 
 }
 
 # 2. Subscribe Your Email to the Hub
@@ -11,3 +14,4 @@ resource "aws_sns_topic_subscription" "email_alerts" {
   # This automatically pulls the email from your GitHub Secrets (TF_VAR_security_alert_email)
   endpoint = var.security_alert_email
 }
+
