45dcdc4e189e7fb9246c1ae7f239d9710386d74b
Nikhil <nikhilnareddula@gmail.com>
2026-03-29 15:22:37 +0530

    fix: format issue for iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -25,7 +25,7 @@ resource "aws_iam_role" "remediation_role" {
 resource "aws_iam_role_policy" "remediation_policy" {
   count = var.is_enabled ? 1 : 0 # THE SAFETY SWITCH
   name  = "zero-tolerance-ec2-policy"
-  role = aws_iam_role.remediation_role[0].id
+  role  = aws_iam_role.remediation_role[0].id
 
   policy = jsonencode({
     Version = "2012-10-17"
